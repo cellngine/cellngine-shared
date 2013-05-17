eb22499d061fca297d41508d8b3126c1ce4feed4
A.J.A. Boer <jboer@jboer.nl>
2013-05-17 07:14:57 +0200

    Modified the encrypt and decrypt methods to return the modified byte array. (The input byte array should no longer be modified.)

--- src/main/java/com/cellngine/crypto/StreamCipher.java
+++ src/main/java/com/cellngine/crypto/StreamCipher.java
@@ -3,30 +3,34 @@ package com.cellngine.crypto;
 /**
  * An interface all symmetric, byte-oriented stream ciphers should implement. The key used for en-
  * and decryption should be passed to the constructor of the specific implementation.
- * 
+ *
  * Implementations of this interface are not required to be thread-safe.
- * 
+ *
  * @author qwertty <hellraz0r.386@googlemail.com>
  */
 public interface StreamCipher
 {
 	/**
-	 * Encrypts a given input.
-	 * 
+	 * Encrypts a given input.<br /><br />
+	 *
+	 * The input byte array is not modified by calls to this function.
+	 *
 	 * @param input
 	 *            The plain- or ciphertext that shall be encrypted.
 	 * @return The result of the operation.
 	 * @throws <code>NullPointerException</code> if <code>input</code> is <code>null</code>.
 	 */
-	public void encrypt(final byte[] input);
-	
+	public byte[] encrypt(final byte[] input);
+
 	/**
-	 * Decrypts a given input.
-	 * 
+	 * Decrypts a given input.<br /><br />
+	 *
+	 * The input byte array is not modified by calls to this function.
+	 *
 	 * @param input
 	 *            The ciphertext that shall be decrypted.
 	 * @return The result of the operation.
 	 * @throws <code>NullPointerException</code> if <code>input</code> is <code>null</code>.
 	 */
-	public void decrypt(final byte[] input);
+	public byte[] decrypt(final byte[] input);
 }
